ca8fb6a5429204174365dff67e2d91e4a0f224ce
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-05-24 15:54:03 +0530

    something wasnot working; said someethign was wrong with faactory; will have to take this out bit by bit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,72 @@
+// import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+
+// import { SocialLoginModule } from 'angularx-social-login';
+// import { AuthServiceConfig, GoogleLoginProvider } from 'angularx-social-login';
+
+// import {MatCardModule} from '@angular/material/card';
+
+// import { AppComponent } from './app.component';
+// import { LoginComponent } from './login/login.component';
+
+// const config = new AuthServiceConfig([
+//   {
+//     id: GoogleLoginProvider.PROVIDER_ID,
+//     provider: new GoogleLoginProvider('528961187921-ld24b25466u4t2lacn9r35asg000lfis.apps.googleusercontent.com')
+//   }
+// ]);
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     LoginComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     SocialLoginModule,
+//     MatCardModule
+//     ],
+//   providers: [
+//     {
+//       provide: AuthServiceConfig,
+//       useFactory: config
+//     }
+//   ],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { SocialLoginModule } from 'angularx-social-login';
-import { AuthServiceConfig, GoogleLoginProvider } from 'angularx-social-login';
-
-import {MatCardModule} from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
+import { SocialLoginModule } from 'angularx-social-login';
+import { AuthServiceConfig, GoogleLoginProvider, FacebookLoginProvider, LinkedInLoginProvider  } from 'angularx-social-login';
+
+
 const config = new AuthServiceConfig([
   {
     id: GoogleLoginProvider.PROVIDER_ID,
-    provider: new GoogleLoginProvider('528961187921-ld24b25466u4t2lacn9r35asg000lfis.apps.googleusercontent.com')
-  }
+    provider: new GoogleLoginProvider('368729150840-rl60t01f22rqirjmntq72es1qchusk4d.apps.googleusercontent.com')
+  },
+  // {
+  //   id: FacebookLoginProvider.PROVIDER_ID,
+  //   provider: new FacebookLoginProvider('561602290896109')
+  // },
+  // {
+  //   id: LinkedInLoginProvider.PROVIDER_ID,
+  //   provider: new LinkedInLoginProvider("78iqy5cu2e1fgr")
+  // }
 ]);
+
+export function provideConfig() {
+  return config;
+}
 
 @NgModule({
   declarations: [
@@ -23,13 +75,14 @@ const config = new AuthServiceConfig([
   ],
   imports: [
     BrowserModule,
-    SocialLoginModule,
-    MatCardModule
-    ],
+    FormsModule,
+    HttpModule,
+    SocialLoginModule
+  ],
   providers: [
     {
       provide: AuthServiceConfig,
-      useFactory: config
+      useFactory: provideConfig
     }
   ],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,45 @@
+// import { Component, OnInit } from '@angular/core';
+// import { SocialUser, AuthService, GoogleLoginProvider } from 'angularx-social-login';
+
+// @Component({
+//   selector: 'app-login',
+//   templateUrl: './login.component.html',
+//   styleUrls: ['./login.component.css']
+// })
+// export class LoginComponent implements OnInit {
+
+//   user: SocialUser;
+
+//   constructor(private authService: AuthService) { }
+
+//   ngOnInit() {
+//     this.authService.authState.subscribe((user) => {
+//       this.user = user;
+//       console.log(user);
+//     });
+//   }
+
+//   // initiateGoolgeAuth(){
+//   //   this.authService.signIn(GoogleLoginProvider.PROVIDER_ID)
+//   //     .then((user) => {
+//   //       // this.user = user;
+//   //       console.log(user)
+//   //     });
+//   // }
+
+//   // signOut(): void {
+//   //   this.authService.signOut();
+//   // }
+
+// }
+
+
 import { Component, OnInit } from '@angular/core';
+
+import { AuthService } from 'angularx-social-login';
+import { SocialUser } from 'angularx-social-login';
+import { GoogleLoginProvider, FacebookLoginProvider, LinkedInLoginProvider } from 'angularx-social-login';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +48,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  user: SocialUser;
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      console.log(user);
+    });
+  }
+
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID).then(x => console.log(x));
+  }
+
+  signInWithFB(): void {
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID).then(x => console.log(x));
+  }
+
+  signInWithLinkedIn(): void {
+    this.authService.signIn(LinkedInLoginProvider.PROVIDER_ID).then(x => console.log(x));
+  }
+
+  signOut(): void {
+    this.authService.signOut();
   }
 
 }
